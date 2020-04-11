65f8bf980d7445ec2e429182177d3519b0eae59d
David <davie668@gmail.com>
2020-04-11 07:40:02 +0100

    Fix application model

--- enable_uuid/template.rb
+++ enable_uuid/template.rb
@@ -1,5 +1,5 @@
 copy_file "enable_uuid/db/migrate/00000000000000_enable_uuid.rb", "db/migrate/00000000000000_enable_uuid.rb"
 copy_file "enable_uuid/config/initializers/generators.rb", "config/initializers/generators.rb"
 insert_into_file "app/models/application_record.rb",
-                 "\nself.implicit_order_column = :created_at'\n",
+                 "\nself.implicit_order_column = :created_at\n",
                  after: "self.abstract_class = true"
